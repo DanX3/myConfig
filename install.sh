ff0e49fe80469a9d0fcfd131ba73f34005ae1306
Daniele <daniel.tolomelli@gmail.com>
2016-09-29 22:30:22 +0200

    refactored font directories

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 sudo apt-get install vim
 sudo apt-get install terminator
-git config --global user.email daniele.tolomelli@gmail.com
+git config --global user.email daniel.tolomelli@gmail.com
 git config --global user.name Daniele Tolomelli
 sudo apt-get install gparted
 sudo apt-get install clementine 
 sudo apt-get install mplayer 
+ln -s /sys/class/backlight/*/brightness ~/brightness
+ln -s /sys/class/power_supply/*/capacity ~/battery
